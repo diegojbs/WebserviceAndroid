8b2d25eb73d8d34426d23c3524e3e5b860e9823f
diegojbs <diego-jbs@hotmail.com>
2016-08-07 13:21:08 -0500

    76.- Agregar metodo que reciba usuario y contraseña

--- app/src/main/java/com/mastotal/webserv/ejemplowebservicecf/HttpManager.java
+++ app/src/main/java/com/mastotal/webserv/ejemplowebservicecf/HttpManager.java
@@ -1,5 +1,7 @@
 package com.mastotal.webserv.ejemplowebservicecf;
 
+import android.util.Base64;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,6 +17,47 @@ public class HttpManager {
     // El siguiente metodo va a traer los datos
     public static String getData(String uri){
         BufferedReader reader = null;
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            StringBuilder stringBuilder = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // nos permitira ir leyendo lo que traemos
+            String line;
+
+            while ((line = reader.readLine())!= null){
+                stringBuilder.append(line + "\n");
+
+            }
+
+            return stringBuilder.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally { //cerrando
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return  null;
+                }
+            }
+        }
+
+    }
+
+    // El siguiente metodo va a traer los datos
+    public static String getData(String uri, String username, String password){
+        BufferedReader reader = null;
+        //inicio codigo para entrar al webservice con seguridad
+        byte[] loginBytes = (username + ":" + password).getBytes();
+        StringBuilder loginBuilder = new StringBuilder()
+                .append("Basic")
+                .append(Base64.encodeToString(loginBytes, Base64.DEFAULT));
+        //fin codigo para entrar al webservice con seguridad
+
         try {
             URL url = new URL(uri);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
