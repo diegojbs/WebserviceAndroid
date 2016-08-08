32c5b95005b117dd302f96807c35e019d3717751
diegojbs <diego-jbs@hotmail.com>
2016-08-07 19:55:02 -0500

    104.- Insertar parámetros

--- app/src/main/java/com/mastotal/webserv/ejemplowebservicecf/RequestPackage.java
+++ app/src/main/java/com/mastotal/webserv/ejemplowebservicecf/RequestPackage.java
@@ -32,6 +32,12 @@ public class RequestPackage {
     }
 
     public void setParams(Map<String, String> params) {
+
         this.params = params;
+    }
+
+    public void setParam(String key, String value) {
+
+        params.put(key,value);
     }
 }
