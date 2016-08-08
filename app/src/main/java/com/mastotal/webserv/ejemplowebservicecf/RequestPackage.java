1aa7aa0085d105afda09412ee6dd7ac90bd290ee
diegojbs <diego-jbs@hotmail.com>
2016-08-07 19:44:26 -0500

    103.- Crear clase para envio de datos

---
+++ app/src/main/java/com/mastotal/webserv/ejemplowebservicecf/RequestPackage.java
@@ -0,0 +1,37 @@
+package com.mastotal.webserv.ejemplowebservicecf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by diego on 7/08/2016.
+ */
+public class RequestPackage {
+    private String uri;
+    private String method = "GET";
+    private Map<String, String> params = new HashMap<>();
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+}
